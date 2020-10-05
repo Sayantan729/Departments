8d9f8024b1d8efb961487a3d7bde6e7c4c602780
Sayantan729 <sayantanbarik729@gmail.com>
2020-10-05 18:56:32 +0530

    None

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecordsComponent } from './records/records.component';
+import { RecordRowComponent } from './record-row/record-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordsComponent,
+    RecordRowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/record-row/record-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-record-row',
+  templateUrl: './record-row.component.html',
+  styleUrls: ['./record-row.component.css']
+})
+export class RecordRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
